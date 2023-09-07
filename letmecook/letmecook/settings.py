ae259e2e0032f0e2048b777b895ebefa342c4297
wardbox <dylank@startmail.com>
2023-09-06 22:06:50 -0700

    remove staticfiles dirs when using collectstatic

--- letmecook/letmecook/settings.py
+++ letmecook/letmecook/settings.py
@@ -157,9 +157,9 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "/static/"
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
+# STATICFILES_DIRS = [
+#     BASE_DIR / "static",
+# ]
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 
