b5c68e77d8c245a5fb5dccda2152cff2acab8c48
wardbox <dylank@startmail.com>
2024-07-19 21:18:51 -0700

    remove all djano stuff

--- letmecook/recipes/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = "recipes"
-urlpatterns = [
-    path("<int:recipe_id>/", views.recipe_detail, name="recipe_detail"),
-    path("", views.recipe_list, name="home"),
-]
