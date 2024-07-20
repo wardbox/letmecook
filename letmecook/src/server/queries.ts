cf786feb075f8d6b808ed3e0a60e65fa59271748
wardbox <dylank@startmail.com>
2024-07-19 23:01:55 -0700

    add recipe model and POC for querying

--- letmecook/src/server/queries.ts
+++ letmecook/src/server/queries.ts
@@ -1,1 +1,17 @@
 import { HttpError } from "wasp/server";
+import {
+  type Recipe,
+} from "wasp/entities"
+
+import {
+  type GetAllRecipes,
+} from "wasp/server/operations"
+
+
+export const getAllRecipes = (async (_args, context) => {
+  const recipes = await context.entities.Recipe.findMany({
+    include: { steps: true, ingredients: true },
+    orderBy: { updatedAt: "desc" },
+  })
+  return recipes;
+}) satisfies GetAllRecipes
