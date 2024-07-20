60e10b211d59a3c77d2c99791c3a2aab4cfc9b51
wardbox <dylank@startmail.com>
2024-07-19 22:26:12 -0700

    add dark mode

--- letmecook/src/client/App.tsx
+++ letmecook/src/client/App.tsx
@@ -2,17 +2,20 @@ import React, { FormEventHandler, FormEvent } from "react";
 import { type AuthUser, getUsername } from "wasp/auth";
 import { logout } from "wasp/client/auth";
 import "./Main.css";
+import { ThemeProvider } from "../components/theme-provider";
 
 export default function App({ children }: { children: React.ReactNode }) {
   return (
-    <div>
-      <header>
-        <h1 className="text-3xl font-bold underline">letmecook</h1>
-      </header>
-      {children}
-      <footer>
-        <p>footer</p>
-      </footer>
-    </div>
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <div>
+        <header>
+          <h1 className="text-3xl font-bold underline">letmecook</h1>
+        </header>
+        {children}
+        <footer>
+          <p>footer</p>
+        </footer>
+      </div>
+    </ThemeProvider>
   )
 }
--- letmecook/src/client/app/LandingPage.tsx
+++ letmecook/src/client/app/LandingPage.tsx
@@ -1,5 +1,5 @@
 import { Button } from "../../components/ui/button";
-
+import { ModeToggle } from "../../components/mode-toggle";
 export default function LandingPage() {
   return (
     <div>
