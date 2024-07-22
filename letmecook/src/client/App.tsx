ae416c7a18ce2da74550040960126e5a0ed5b787
wardbox <dylank@startmail.com>
2024-07-21 21:51:35 -0700

    functionality for print and share buttons

--- letmecook/src/client/App.tsx
+++ letmecook/src/client/App.tsx
@@ -1,13 +1,18 @@
 import React from "react";
 import "./Main.css";
+import "./print.css";
 import { ThemeProvider } from "../components/theme-provider";
+import {
+  Toaster
 
+} from "../components/ui/toaster";
 export default function App({ children }: { children: React.ReactNode }) {
   return (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
       <main id="root" className="h-screen antialiased">
         {children}
       </main>
+      <Toaster />
     </ThemeProvider>
   )
 }
--- letmecook/src/components/Recipe/RecipePage.tsx
+++ letmecook/src/components/Recipe/RecipePage.tsx
@@ -3,9 +3,29 @@ import { useQuery, getRecipe } from "wasp/client/operations";
 import { Button } from "../ui/button";
 import { Badge } from "../ui/badge";
 import { ArrowUp, ArrowDown, Printer, Share } from "@phosphor-icons/react";
+import { toast, useToast } from "../../components/ui/use-toast"
+
 
 export default function RecipePage(props: RouteComponentProps<{ id: string }>) {
   const { data, error, isLoading } = useQuery(getRecipe, { recipeId: props.match.params.id });
+  const url = location.href;
+
+  const handleClickShare = () => {
+    try {
+      navigator.clipboard.writeText(url)
+      toast({
+        title: "Copied to clipboard",
+      })
+    } catch (error) {
+      toast({
+        title: "Failed to copy to clipboard",
+      })
+    }
+  }
+
+  const handlePrintRecipe = () => {
+    window.print()
+  }
 
   return (
     <div className="h-full max-w-3xl mx-auto p-8">
@@ -14,27 +34,27 @@ export default function RecipePage(props: RouteComponentProps<{ id: string }>) {
           <section id="heading" className="p-3">
             <hgroup className="flex flex-col gap-5">
               <h1 className="text-6xl font-bold">{data.title}</h1>
-              <div className="flex flex-wrap gap-2">
+              <div className="flex flex-wrap gap-2 printhide">
                 {data.tags.map((tag) => (
                   <Badge key={tag.id} variant="secondary">#{tag.name}</Badge>
                 ))}
               </div>
-              <div className="flex gap-2 items-center justify-between">
+              <div className="flex gap-2 items-center justify-between printhide">
                 <div className="flex gap-3">
                   <Button>{data.upvotes}<ArrowUp size={16} /></Button>
                   <Button variant="secondary">{data.downvotes}<ArrowDown size={16} /></Button>
                 </div>
                 <div className="flex gap-3">
-                  <Button><Printer size={24} /></Button>
-                  <Button><Share size={24} /></Button>
+                  <Button onClick={handlePrintRecipe}><Printer size={24} /></Button>
+                  <Button onClick={handleClickShare}><Share size={24} /></Button>
                 </div>
               </div>
             </hgroup>
           </section>
-          <section id="photos">
+          <section id="photos" className="printhide">
             <img className="rounded-sm" src="https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F43%2F2022%2F11%2F15%2F6777730-chicken-noodle-salad-Chef-John-1x1-1.jpg&q=60&c=sc&poi=auto&orient=true&h=512" alt="photo of food" />
           </section>
-          <hr />
+          <hr className="printhide" />
           <section id="logistics" className="flex flex-col gap-3 max-w-max">
             <h2 className="text-3xl font-bold">Logistics</h2>
             <div className="grid grid-cols-2 gap-3">
